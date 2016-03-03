24102da2ddabd6c90ca022293659902983ac3844
Alex <alex.barry@gmail.com>
2016-03-02 19:23:27 -0500

    Updated server to use regular node rather than babel which took an unreasonable amount of time to load up.

--- server/index.js
+++ server/index.js
@@ -1,21 +1,19 @@
-import path from 'path';
-import express from 'express';
-import webpack from 'webpack';
-import webpackMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import config from '../config/webpack.development.js';
+var path = require('path'),
+    express = require('express'),
+    webpack = require('webpack'),
+    webpackMiddleware = require('webpack-dev-middleware'),
+    webpackHotMiddleware = require('webpack-hot-middleware'),
+    config = require('../config/webpack.hmr.js'),
+    isDeveloping = (process.env.NODE_ENV !== 'production'),
+    port = (isDeveloping ? 3000 : process.env.PORT),
+    app = express(),
+    rootPath = path.join(__dirname, '..', 'public')
 
-const isDeveloping = process.env.NODE_ENV !== 'production';
-const port = isDeveloping ? 3000 : process.env.PORT;
-const app = express();
-
-const rootPath = path.join(__dirname, '..', 'public')
-
-app.use(express.static(rootPath));
+app.use(express.static(rootPath))
 
 if (isDeveloping) {
   console.log('--- DEV MODE ---')
-  const compiler = webpack(config);
+  const compiler = webpack(config)
   const middleware = webpackMiddleware(compiler, {
     publicPath: config.output.publicPath,
     stats: {
@@ -26,28 +24,28 @@ if (isDeveloping) {
       chunkModules: false,
       modules: false
     }
-  });
+  })
 
-  app.use(middleware);
-  app.use(webpackHotMiddleware(compiler));
+  app.use(middleware)
+  app.use(webpackHotMiddleware(compiler))
 
   app.get('*', function response(req, res) {
     const defaultFile = path.join(rootPath, 'index.html')
 
-    res.write(middleware.fileSystem.readFileSync(defaultFile));
-    res.end();
-  });
+    res.write(middleware.fileSystem.readFileSync(defaultFile))
+    res.end()
+  })
 
 } else {
   app.get('*', function response(req, res) {
     const defaultFile = path.join(rootPath, 'index.html')
-    res.sendFile(defaultFile);
-  });
+    res.sendFile(defaultFile)
+  })
 }
 
 app.listen(port, '0.0.0.0', function onStart(err) {
   if (err) {
-    console.log(err);
+    console.log(err)
   }
-  console.info('==> 🌎  Listening on http://0.0.0.0:%s/ in your browser.', port);
-});
+  console.info('==> 🌎  Listening on http://0.0.0.0:%s/ in your browser.', port)
+})
