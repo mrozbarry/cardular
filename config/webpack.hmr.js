a7a0730f84e737fcecc3827b783a59160fc150f0
Alex <alex.barry@gmail.com>
2016-03-02 19:20:11 -0500

    Updated webpack config schemes, renamed them to be more meaningful, added basic firebase rules in a bolt file.

---
+++ config/webpack.hmr.js
@@ -0,0 +1,57 @@
+const Webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ROOT = path.join(__dirname, '..')
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(ROOT, 'client', 'router', 'index.js'),
+  ],
+
+  output: {
+    path: path.join(ROOT, 'public'),
+    publicPath: '',
+    filename: 'entry.js'
+  },
+
+  module: {
+    preLoaders: [{
+      test: /\.jsx?$/,
+      loader: 'standard',
+      exclude: /(node_modules|bower_components)/
+    }],
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.json$/, loader: 'json' },
+      { test: /\.html$/, loader: 'html' }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.css'],
+    root: [
+      path.join(ROOT, 'client', 'router'),
+      path.join(ROOT, 'client', 'shared'),
+      path.join(ROOT, 'client', 'profiles'),
+      path.join(ROOT, 'client', 'join'),
+      path.join(ROOT, 'client', 'game'),
+      path.join(ROOT, 'client', 'admin')
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: 'head',
+      template: path.join(ROOT, 'templates', 'index.html')
+    }),
+    new Webpack.HotModuleReplacementPlugin(),
+    new Webpack.optimize.OccurenceOrderPlugin(),
+    new Webpack.NoErrorsPlugin()
+  ]
+};
--- config/webpack.static.js
+++ config/webpack.static.js
@@ -1,17 +1,17 @@
 const Webpack = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ROOT = path.join(__dirname, '..')
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
 
   entry: [
-    'webpack-hot-middleware/client?reload=true',
-    path.join(__dirname, '..', 'client', 'router', 'index.js'),
+    path.join(ROOT, 'client', 'router', 'index.js'),
   ],
 
   output: {
-    path: path.join(__dirname, '..', 'public'),
+    path: path.join(ROOT, 'public'),
     publicPath: '',
     filename: 'entry.js'
   },
@@ -27,22 +27,27 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel'
       },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.css$/i, loader: 'style-loader!css-loader' },
       { test: /\.json$/, loader: 'json' },
       { test: /\.html$/, loader: 'html' }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx', '.css']
+    extensions: ['', '.js', '.jsx', '.css'],
+    root: [
+      path.join(ROOT, 'client', 'router'),
+      path.join(ROOT, 'client', 'shared'),
+      path.join(ROOT, 'client', 'profiles'),
+      path.join(ROOT, 'client', 'join'),
+      path.join(ROOT, 'client', 'game'),
+      path.join(ROOT, 'client', 'admin')
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      inject: 'head',
-      template: path.join(__dirname, '..', 'templates', 'index.html')
+      inject: 'head'
     }),
-    new Webpack.HotModuleReplacementPlugin(),
-    new Webpack.optimize.OccurenceOrderPlugin(),
-    new Webpack.NoErrorsPlugin()
+    new Webpack.optimize.OccurenceOrderPlugin()
   ]
 };
